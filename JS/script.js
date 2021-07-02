141cd6c087fd4257975abaa8ece9266505c26507
SRHembree <75818806+SRHembree@users.noreply.github.com>
2021-07-02 18:56:23 -0400

    Copied L5P pages over as basis for this page

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    var x = document.getElementById("navbar-collapse")
+    if (x.style.display === "block") {
+        x.style.display = "none"; 
+    } else {
+        x.style.display = "block";
+    }
+}
+function newFunction() {
+    var x = document.getElementById("tableOfContents")
+    if (x.style.display === "block") {
+        x.style.display = "none"; 
+    } else {
+        x.style.display = "block"
+    }
+}
